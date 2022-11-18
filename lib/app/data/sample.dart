f954111c114c4a8ce5fec0be52fbda0262837972
Anuchit Chalothorn <anoochit@gmail.com>
2022-11-18 18:59:19 +0700

    clean sample data

--- lib/app/data/sample.dart
+++ lib/app/data/sample.dart
@@ -1,25 +1,110 @@
-import 'package:winapp/app/data/order.dart';
 import 'package:winapp/app/data/product.dart';
 
 List<Product> sampleProducts = [
-  Product(id: "1", title: "ข้าวผัดหมู", image: "https://via.placeholder.com/150", categoryId: "1", price: 50.00),
   Product(
-      id: "2", title: "ข้าวผัดคะน้าปลาเค็ม", image: "https://via.placeholder.com/150", categoryId: "1", price: 50.00),
-  Product(id: "3", title: "ข้าวผัดปลากระป่อง", image: "https://via.placeholder.com/150", categoryId: "1", price: 40.00),
-  Product(id: "4", title: "ข้าวคะน้าหมูกรอบ", image: "https://via.placeholder.com/150", categoryId: "1", price: 50.00),
-  Product(id: "5", title: "ข้าวหมูทอด", image: "https://via.placeholder.com/150", categoryId: "1", price: 40.00),
-  Product(id: "6", title: "ข้าวไข่เจียว", image: "https://via.placeholder.com/150", categoryId: "1", price: 40.00),
-  Product(id: "7", title: "น้ำอัดลม", image: "https://via.placeholder.com/150", categoryId: "2", price: 25.00),
-  Product(id: "8", title: "น้ำเปล่า", image: "https://via.placeholder.com/150", categoryId: "2", price: 15.00),
-  Product(id: "9", title: "น้ำแข็ง", image: "https://via.placeholder.com/150", categoryId: "2", price: 5.00),
-  Product(id: "10", title: "ก๋วยเตี๋ยว", image: "https://via.placeholder.com/150", categoryId: "3", price: 40.00),
-  Product(id: "11", title: "เย็นตาโฟ", image: "https://via.placeholder.com/150", categoryId: "3", price: 50.00),
-  Product(id: "12", title: "เกาหลา", image: "https://via.placeholder.com/150", categoryId: "3", price: 50.00),
-  Product(id: "13", title: "ข้าวเปล่า", image: "https://via.placeholder.com/150", categoryId: "3", price: 20.00)
+    id: "1",
+    title: "ข้าวผัดหมู",
+    image: "https://via.placeholder.com/150",
+    categoryId: "1",
+    price: 50.00,
+  ),
+  Product(
+    id: "2",
+    title: "ข้าวผัดคะน้าปลาเค็ม",
+    image: "https://via.placeholder.com/150",
+    categoryId: "1",
+    price: 50.00,
+  ),
+  Product(
+    id: "3",
+    title: "ข้าวผัดปลากระป่อง",
+    image: "https://via.placeholder.com/150",
+    categoryId: "1",
+    price: 40.00,
+  ),
+  Product(
+    id: "4",
+    title: "ข้าวคะน้าหมูกรอบ",
+    image: "https://via.placeholder.com/150",
+    categoryId: "1",
+    price: 50.00,
+  ),
+  Product(
+    id: "5",
+    title: "ข้าวหมูทอด",
+    image: "https://via.placeholder.com/150",
+    categoryId: "1",
+    price: 40.00,
+  ),
+  Product(
+    id: "6",
+    title: "ข้าวไข่เจียว",
+    image: "https://via.placeholder.com/150",
+    categoryId: "1",
+    price: 40.00,
+  ),
+  Product(
+    id: "7",
+    title: "น้ำอัดลม",
+    image: "https://via.placeholder.com/150",
+    categoryId: "2",
+    price: 25.00,
+  ),
+  Product(
+    id: "8",
+    title: "น้ำเปล่า",
+    image: "https://via.placeholder.com/150",
+    categoryId: "2",
+    price: 15.00,
+  ),
+  Product(
+    id: "9",
+    title: "น้ำแข็ง",
+    image: "https://via.placeholder.com/150",
+    categoryId: "2",
+    price: 5.00,
+  ),
+  Product(
+    id: "10",
+    title: "ก๋วยเตี๋ยว",
+    image: "https://via.placeholder.com/150",
+    categoryId: "3",
+    price: 40.00,
+  ),
+  Product(
+    id: "11",
+    title: "เย็นตาโฟ",
+    image: "https://via.placeholder.com/150",
+    categoryId: "3",
+    price: 50.00,
+  ),
+  Product(
+    id: "12",
+    title: "เกาหลา",
+    image: "https://via.placeholder.com/150",
+    categoryId: "3",
+    price: 50.00,
+  ),
+  Product(
+    id: "13",
+    title: "ข้าวเปล่า",
+    image: "https://via.placeholder.com/150",
+    categoryId: "3",
+    price: 20.00,
+  )
 ];
 
 List<Category> sampleCategory = [
-  Category(id: "1", title: "ตามสั่ง"),
-  Category(id: "3", title: "ก๋วยเตี๋ยว"),
-  Category(id: "2", title: "เครื่องดื่ม"),
+  Category(
+    id: "1",
+    title: "ตามสั่ง",
+  ),
+  Category(
+    id: "3",
+    title: "ก๋วยเตี๋ยว",
+  ),
+  Category(
+    id: "2",
+    title: "เครื่องดื่ม",
+  ),
 ];
